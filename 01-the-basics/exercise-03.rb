e84698cfce4b48d6e179bf325d01b5bc7ea1dcfd
Jake Boyce <thisisjakeboyce@gmail.com>
2019-02-27 10:31:08 -0500

    Implement exercise 3 of chapter 1

---
+++ 01-the-basics/exercise-03.rb
@@ -0,0 +1,13 @@
+movies = {
+  :fight_club => 1999,
+  :donnie_darko => 2001,
+  :avengers_infinity_war => 2018,
+  :toy_story_3 => 2010,
+  :interstellar => 2014
+}
+
+puts movies[:fight_club]
+puts movies[:donnie_darko]
+puts movies[:avengers_infinity_war]
+puts movies[:toy_story_3]
+puts movies[:interstellar]
